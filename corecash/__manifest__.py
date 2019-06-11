14410e6dce9862838c5350521523d65153389fd2
Yennifer <yennifer@vauxoo.com>
2019-06-11 09:18:22 -0400

    [ADD] corecash: Initialize app for customizations

---
+++ corecash/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Corecash App',
+    'version': '12.0.1.0.0',
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    'category': 'Hidden',
+    'summary': 'Corecash App for customizations',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}

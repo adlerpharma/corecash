5ec88880210cc9707947f30615977560142a212a
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2021-04-01 18:23:42 -0600

    Update __manifest__.py

--- corecash/__manifest__.py
+++ corecash/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Corecash App',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     "author": "Vauxoo",
     "license": "LGPL-3",
     'category': 'Hidden',
